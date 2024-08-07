06eeb14b4ddc523cfdc2fcd4f66a5d5bedb85699
Striver11-Praveen Kumar <83543044+striver11@users.noreply.github.com>
2024-08-07 13:19:49 +0000

    1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,58 @@
+
+
+
+// Feature	           ||           var	                           ||               let	                         ||          const
+
+// Scope	           ||   Function-scoped	                       ||           Block-scoped                     ||       Block-scoped
+// Re-declaration	   ||  Allowed within the same scope	       ||  Not allowed within the same scope	     ||  Not allowed within the same scope
+// Re-assignment	   ||       Allowed                            ||            	Allowed	                     ||   Not allowed (must be initialized)
+// Initialization	   || Can be declared without initialization   ||   	Must be initialized upon declaration ||  	Must be initialized
+//  TemporalDead Zone  ||Does not have a temporal dead zone        ||           Has a temporal dead zone	     ||     Has a temporal dead zone
+
+
+console.log("+++++++++++++++++  var example ++++++++++++++++++++++++++")
+function exampleVar() {
+    console.log(x); // undefined
+    var x = 10;
+    if (true) {
+        var x = 20; // same variable, function-scoped
+        console.log(x); // 20
+    }
+    console.log(x); // 20
+}
+exampleVar();
+
+
+
+console.log("-----------------------------  let example -------------------------------")
+function exampleLet() {
+    //console.log(x); // ReferenceError: Cannot access 'x' before initialization
+    let x = 10;
+    if (true) {
+        let x = 20; // different variable, block-scoped
+        console.log(x); // 20
+    }
+    console.log(x); // 10
+}
+exampleLet();
+
+
+console.log("*******************************  const example  ****************************")
+function exampleConst() {
+   // console.log(x); // ReferenceError: Cannot access 'x' before initialization
+    const x = 10;
+    if (true) {
+        const x = 20; // different variable, block-scoped
+        console.log(x); // 20
+    }
+    console.log(x); // 10
+}
+exampleConst();
+
+// Example with object
+const obj = { key: 'value' };
+obj.key = 'newValue'; // Allowed
+// obj = {}; // TypeError: Assignment to constant variable.
+
+
+
